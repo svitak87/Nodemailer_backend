5b4d755a750267622f8e363910ccfe2a65f1c04c
svitak87 <svitakwe87@hotmail.com>
2024-07-31 18:46:10 -0300

    cors

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ const cors = require("cors");
 const server = express();
 const PORT = process.env.PORT || 3001;
 
-// para subir
 server.use(morgan("dev"));
-server.use(cors()); 
+server.use(cors());
 server.use(express.json());
 
+// Asegúrate de que esta línea maneje la ruta '/contact'
 server.use('/contact', require('./routes/contact'));
 
+// Manejo de ruta raíz para pruebas
+server.get('/', (req, res) => {
+  res.send('Servidor en ejecución');
+});
 
-const main = () => {
-    server.listen(PORT, () => {
-        console.log(`Server is running on port: http://localhost:${PORT}`);
-    });
-};
+server.listen(PORT, () => {
+  console.log(`Server is running on port: http://localhost:${PORT}`);
+});
 
-main();
