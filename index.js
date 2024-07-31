9147cf8f522b8cc426eb31ea75f97be2187ea7dc
svitak87 <svitakwe87@hotmail.com>
2024-07-31 16:54:09 -0300

    cors

--- index.js
+++ index.js
@@ -5,15 +5,9 @@ const cors = require("cors");
 const server = express();
 const PORT = process.env.PORT || 3001;
 
-// Configuración de CORS
-const corsOptions = {
-  origin: 'http://localhost:4000', // Permite solicitudes desde localhost:4000
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type']
-};
 
 server.use(morgan("dev"));
-server.use(cors(corsOptions)); // Aplica la configuración de CORS
+server.use(cors({ origin: '*' }));
 server.use(express.json());
 
 server.use('/contact', require('./routes/contact'));
