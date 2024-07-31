7f2fc9d432b095c6c0de327046a2d0ff11899900
svitak87 <svitakwe87@hotmail.com>
2024-07-31 16:32:47 -0300

    verify

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ const server = express();
 const PORT = process.env.PORT || 3001;
 
 
+const corsOptions = {
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+  allowedHeaders: ["Content-Type"], 
+};
+
 server.use(morgan("dev"));
-server.use(cors())
-server.use(express.json())
+server.use(cors(corsOptions));
+server.use(express.json());
+
 
 server.use('/contact', require('./routes/contact'));
 
+
 const main = () => {
     server.listen(PORT, () => {
-        console.log(`server is running on port: http://localhost:${PORT}`)
-    })
-}
+        console.log(`Server is running on port: http://localhost:${PORT}`);
+    });
+};
 
-main()
+main();
